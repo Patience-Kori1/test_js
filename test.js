b4d16e901f39154613d36890493c6e248c7bc65f
Patience-Kori1 <koribiram@yahoo.fr>
2025-06-16 14:02:49 +0200

    Solved exercises 1 and 2 on variable types and declarations in JavaScript

---
+++ test.js
@@ -0,0 +1,31 @@
+ // Exercice 1. Les types de variable en JS
+
+        let a= 0; // number
+        let b= true; // Boolean
+        let c = 2 - 1.2; //number
+        let d ='hello'; // String
+        let e = 'bonjour' + 4; // String
+        let f = { a: 0.4 }; // Object 
+        let g = [ 'a', 'b', 'c' ]; // Array, Object
+
+        console.log(typeof(g));
+
+        // Exercice 2. Sur la déclaration des variables
+
+        var monNombre = 4;
+        var maChaine = 'bonjour ! :-)';
+        var monBooleen = false;
+        var sansValeur;
+
+        var monNombre = 4;
+        var maChaine = 'bonjour ! :-)';
+        var monBooleen = false;
+        var sansValeur;
+
+        // Vérification des variables
+        console.log(monNombre === 4); // Devrait afficher true si monNombre vaut 4
+        console.log(maChaine === 'bonjour ! :-)'); // Devrait afficher true si maChaine contient 'bonjour ! :-)'
+        console.log(monBooleen === false); // Devrait afficher true si monBooleen vaut false
+        console.log(sansValeur === undefined); //
+
+        
