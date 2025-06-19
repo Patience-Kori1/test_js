3991959cb5a3954f541a47af57b072e5ed863ee6
Patience-Kori1 <koribiram@yahoo.fr>
2025-06-19 23:31:29 +0200

    Learning conditional structures with switch case

--- doc.js
+++ doc.js
@@ -17,27 +17,98 @@ let coursesFaites = false;
     };
 ///////////////////////////////////////////////////////////////////
 
+// Théorie Doc sur les Instruction condtionnelles avec if...else if, else
+
+// const select = document.querySelector("select");
+// const para = document.querySelector("p");
+
+// select.addEventListener("change", setWeather);
+
+// function setWeather() {
+//   const choice = select.value;
+
+// if (choice === "sunny") {
+//     para.textContent =
+//       "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+//   } else if (choice === "rainy") {
+//     para.textContent =
+//       "Rain is falling outside; take a rain coat and a brolly, and don't stay out for too long.";
+//   } else if (choice === "snowing") {
+//     para.textContent =
+//       "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+//   } else if (choice === "overcast") {
+//     para.textContent =
+//       "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+//   } else {
+//     para.textContent = "";
+//   }
+// }
+
+// Théorie Doc sur les Instruction condtionnelles avec Switch et Case pour les choix multiples
+
+// switch (expression) {
+//   case choix1:
+//     //exécuter ce code
+//     break;
+
+//   case choix2:
+//     //exécuter ce code à la place
+//     break;
+
+//   // incorporez autant de case que vous le souhaitez
+
+//   default:
+//     // sinon, exécutez juste ce code
+// }
+
+let iiii = 13;
+
+if ( iiii < 10 ) {
+    alert("je suis mineur");
+} else if (iiii > 12 ){
+    alert("je suis majeur")
+    alert(iiii += 4)
+};
 const select = document.querySelector("select");
 const para = document.querySelector("p");
 
 select.addEventListener("change", setWeather);
 
 function setWeather() {
-  const choice = select.value;
+  let choice = select.value;
 
-if (choice === "sunny") {
-    para.textContent =
-      "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
-  } else if (choice === "rainy") {
-    para.textContent =
-      "Rain is falling outside; take a rain coat and a brolly, and don't stay out for too long.";
-  } else if (choice === "snowing") {
-    para.textContent =
-      "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
-  } else if (choice === "overcast") {
-    para.textContent =
-      "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
-  } else {
-    para.textContent = "";
+  switch (choice) {
+    case "sunny":
+      para.textContent =
+        "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+      break;
+    case "rainy":
+      para.textContent =
+        "Rain is falling outside; take a rain coat and a brolly, and don't stay out for too long.";
+      break;
+    case "snowing":
+      para.textContent =
+        "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+      break;
+    case "overcast":
+      para.textContent =
+        "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+      break;
+    default:
+      para.textContent = "";
   }
 }
+
+
+let iii = Number(prompt("")); //J'utilise Number() pour convertir la réponse en nombre parce que prompt est toujours une chaine de caractère
+
+switch (true) {
+    case iii < 10 : alert("Trop petit")
+    break;
+    case iii > 10: alert("Trop grand")
+    break;
+    case iii === 10 : alert("Juste réponse") // Si j'enlève Number() dans iii le strict égale ne marcherait pas dans ce cas pcq 10 !== "10"
+    break;
+    default : alert("Je ne comprends pas ta réponse") 
+}
+// 
